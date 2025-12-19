41cb151043f8684f91e1201c7b0ea5c35a26ca7a
MahmoudShaker15 <mahmoudshaker.rifai@gmail.com>
2025-12-19 18:01:31 +0200

    Add: implement Oval class extend Shape class with overriding draw method

---
+++ src/java/Oval.java
@@ -0,0 +1,37 @@
+import java.awt.*;
+
+public class Oval extends Shape {
+    
+    // Attributes
+    int width, height;
+
+    // Constructors
+    public Oval(int x, int y, Color color, int width, int height) {
+        super(x, y, color);
+        this.width = width;
+        this.height = height;
+    }
+
+    // Accessor
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    // Method
+    @Override
+    public void draw(Graphics2D g2) {
+        g2.setColor(color);
+
+        if (filled) {
+            g2.fillOval(x, y, width, height);
+        } else {
+            g2.setStroke(makeStroke());
+            g2.drawOval(x, y, width, height);
+        }
+    }
+    
+}
